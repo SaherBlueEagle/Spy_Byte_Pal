ba10ae377ed325d2fb42fbc593be14e229ba50b2
SaherBlueEagle <44013704+SaherBlueEagle@users.noreply.github.com>
2019-05-10 14:34:42 +0200

    Add files via upload

---
+++ Spy_Byte_Pal(Controller)/ServiceController.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ServiceController
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ServiceController))
+        Me.SkyDarkForm1 = New X_Theme.SkyDarkForm()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.StopServiceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RunServiceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RefreshServicesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SkyDarkForm1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SkyDarkForm1
+        '
+        Me.SkyDarkForm1.BackColor = System.Drawing.Color.FromArgb(CType(CType(62, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.SkyDarkForm1.Controls.Add(Me.Label2)
+        Me.SkyDarkForm1.Controls.Add(Me.Label1)
+        Me.SkyDarkForm1.Controls.Add(Me.ListView1)
+        Me.SkyDarkForm1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SkyDarkForm1.Location = New System.Drawing.Point(0, 0)
+        Me.SkyDarkForm1.Name = "SkyDarkForm1"
+        Me.SkyDarkForm1.Size = New System.Drawing.Size(752, 425)
+        Me.SkyDarkForm1.TabIndex = 0
+        Me.SkyDarkForm1.Text = "Services Manager"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(91, 403)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(15, 19)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "-"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(3, 403)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 19)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Services : "
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.FromArgb(CType(CType(62, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.ListView1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.ForeColor = System.Drawing.Color.Lime
+        Me.ListView1.LargeImageList = Me.ImageList1
+        Me.ListView1.Location = New System.Drawing.Point(3, 26)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(746, 374)
+        Me.ListView1.SmallImageList = Me.ImageList1
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Service Status"
+        Me.ColumnHeader1.Width = 222
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Service name"
+        Me.ColumnHeader2.Width = 156
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Service Type"
+        Me.ColumnHeader3.Width = 193
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StopServiceToolStripMenuItem, Me.RunServiceToolStripMenuItem, Me.RefreshServicesToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(159, 70)
+        '
+        'StopServiceToolStripMenuItem
+        '
+        Me.StopServiceToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(62, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.StopServiceToolStripMenuItem.ForeColor = System.Drawing.Color.Lime
+        Me.StopServiceToolStripMenuItem.Image = CType(resources.GetObject("StopServiceToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.StopServiceToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(62, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.StopServiceToolStripMenuItem.Name = "StopServiceToolStripMenuItem"
+        Me.StopServiceToolStripMenuItem.Size = New System.Drawing.Size(158, 22)
+        Me.StopServiceToolStripMenuItem.Text = "Stop Service"
+        '
+        'RunServiceToolStripMenuItem
+        '
+        Me.RunServiceToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(62, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.RunServiceToolStripMenuItem.ForeColor = System.Drawing.Color.Lime
+        Me.RunServiceToolStripMenuItem.Image = CType(resources.GetObject("RunServiceToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.RunServiceToolStripMenuItem.Name = "RunServiceToolStripMenuItem"
+        Me.RunServiceToolStripMenuItem.Size = New System.Drawing.Size(158, 22)
+        Me.RunServiceToolStripMenuItem.Text = "Run Service"
+        '
+        'RefreshServicesToolStripMenuItem
+        '
+        Me.RefreshServicesToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(CType(CType(62, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(58, Byte), Integer))
+        Me.RefreshServicesToolStripMenuItem.ForeColor = System.Drawing.Color.Lime
+        Me.RefreshServicesToolStripMenuItem.Image = CType(resources.GetObject("RefreshServicesToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.RefreshServicesToolStripMenuItem.Name = "RefreshServicesToolStripMenuItem"
+        Me.RefreshServicesToolStripMenuItem.Size = New System.Drawing.Size(158, 22)
+        Me.RefreshServicesToolStripMenuItem.Text = "Refresh Services"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "151.png")
+        Me.ImageList1.Images.SetKeyName(1, "152.png")
+        '
+        'Timer4
+        '
+        Me.Timer4.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Timer3
+        '
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'ServiceController
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(752, 425)
+        Me.Controls.Add(Me.SkyDarkForm1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "ServiceController"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ServiceController"
+        Me.SkyDarkForm1.ResumeLayout(False)
+        Me.SkyDarkForm1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SkyDarkForm1 As X_Theme.SkyDarkForm
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents StopServiceToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RunServiceToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RefreshServicesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ Spy_Byte_Pal(Controller)/ServiceController.vb
@@ -0,0 +1,110 @@
+﻿Public Class ServiceController
+    Dim rr As New TextBox
+
+    Private controllers As New System.Collections.Generic.SortedList(Of String, ServiceController)
+
+    Public sock As Integer
+    Dim state As Integer
+    Dim time As Integer
+
+    Private Sub ServiceController_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        rr.Text = 1
+        Timer3.Enabled = True
+        Main.S.Send(sock, "GOs")
+
+
+
+        If state = 1 Then
+
+            Timer1.Interval = time
+            Timer1.Start()
+        Else
+
+            Timer1.Stop()
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ListView1.Items.Clear()
+
+
+
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+        Dim xt$
+
+        xt = ListView1.Items.Item(ListView1.FocusedItem.Index).SubItems.Item(0).Text()
+        If xt = "Stopped" Then
+            RunServiceToolStripMenuItem.Enabled = True ' run
+            StopServiceToolStripMenuItem.Enabled = False ' stop 
+        End If
+        If xt = "Running" Then
+            RunServiceToolStripMenuItem.Enabled = False ' run
+            StopServiceToolStripMenuItem.Enabled = True  ' stop 
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        On Error Resume Next
+        For Each Process As ListViewItem In ListView1.Items
+
+
+            If Process.Text.Contains("Stopped") Then
+
+                'Process.BackColor = Color.LightSteelBlue
+                Process.ImageIndex = 0
+            ElseIf Process.Text.Contains("Running") Then
+                'Process.BackColor = Color.LightSteelBlue
+                Process.ImageIndex = 1
+            End If
+
+        Next
+
+
+        'Process.BackColor = Color.LightSteelBlue
+
+
+
+    End Sub
+
+    Private Sub StopServiceToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles StopServiceToolStripMenuItem.Click
+        For Each x As ListViewItem In Main.L1.SelectedItems
+            Main.S.Send(x.ToolTipText, "Voz" & "|BlueEagle|" & "0") ' stop
+        Next
+    End Sub
+
+    Private Sub RunServiceToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles RunServiceToolStripMenuItem.Click
+        For Each x As ListViewItem In Main.L1.SelectedItems
+            Main.S.Send(x.ToolTipText, "Voz" & "|BlueEagle|" & "1") ' run
+        Next
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        rr.Text += 1
+
+        If rr.Text = 25 Then
+            Me.Opacity = 9.9R
+
+            Timer3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub RefreshServicesToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles RefreshServicesToolStripMenuItem.Click
+        ListView1.Items.Clear()
+
+        Me.Opacity = 0.8R
+        rr.Text = 1
+        Timer3.Enabled = True
+        Main.S.Send(sock, "GOs")
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Dim C As String
+        C = ListView1.Items.Count.ToString
+        Label2.Text = C
+    End Sub
+End Class
