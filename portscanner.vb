3092fcc65c5d14d3541ca32628ee233cf78fd684
SaherBlueEagle <44013704+SaherBlueEagle@users.noreply.github.com>
2019-05-10 14:05:06 +0200

    Delete portscanner.vb

--- portscanner.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class portscanner
-    Public sock As Integer
-    Public jj As Integer = 0
-
-    Private Sub SkyDarkButton1_Click(sender As System.Object, e As System.EventArgs) Handles SkyDarkButton1.Click
-        ListView1.Items.Clear()
-        Main.S.Send(sock, "scansite" & Main.Yy & TextBox1.Text & Main.Yy & TextBox2.Text & Main.Yy & TextBox3.Text)
-    End Sub
-End Class
